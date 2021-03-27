b0f8129f9a8f4f6db3753c1e846cbf63180eed99
hitwanyang <296866643@qq.com>
2021-03-27 15:51:50 +0800

    ac

---
+++ codeforces/Codeforces Round #710 (Div. 3)/1506e.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @oj: codeforces
+# @id: hitwanyang
+# @email: 296866643@qq.com
+# @date: 2021/3/27 10:30
+# @url: https://codeforc.es/contest/1506/problem/E
+import sys, os
+from io import BytesIO, IOBase
+import collections, itertools, bisect, heapq, math, string
+from decimal import *
+
+# region fastio
+
+BUFSIZE = 8192
+
+BUFSIZE = 8192
+
+
+class FastIO(IOBase):
+	newlines = 0
+
+	def __init__(self, file):
+		self._fd = file.fileno()
+		self.buffer = BytesIO()
+		self.writable = "x" in file.mode or "r" not in file.mode
+		self.write = self.buffer.write if self.writable else None
+
+	def read(self):
+		while True:
+			b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+			if not b:
+				break
+			ptr = self.buffer.tell()
+			self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+		self.newlines = 0
+		return self.buffer.read()
+
+	def readline(self):
+		while self.newlines == 0:
+			b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+			self.newlines = b.count(b"\n") + (not b)
+			ptr = self.buffer.tell()
+			self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+		self.newlines -= 1
+		return self.buffer.readline()
+
+	def flush(self):
+		if self.writable:
+			os.write(self._fd, self.buffer.getvalue())
+			self.buffer.truncate(0), self.buffer.seek(0)
+
+
+class IOWrapper(IOBase):
+	def __init__(self, file):
+		self.buffer = FastIO(file)
+		self.flush = self.buffer.flush
+		self.writable = self.buffer.writable
+		self.write = lambda s: self.buffer.write(s.encode("ascii"))
+		self.read = lambda: self.buffer.read().decode("ascii")
+		self.readline = lambda: self.buffer.readline().decode("ascii")
+
+
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+
+# ------------------------------
+## 注意嵌套括号!!!!!!
+## 先有思路,再写代码,别着急!!!
+## 先有朴素解法,不要有思维定式,试着换思路解决
+## 精度 print("%.10f" % ans)
+## sqrt:int(math.sqrt(n))+1
+## 字符串拼接不要用+操作，会超时
+## 二进制转换:bin(1)[2:].rjust(32,'0')
+## array copy:cur=array[::]
+## oeis(CROSSREFS):example 1, 3, _, 1260, _, _, _, _, _, 12164510040883200
+## sqrt:Decimal(x).sqrt()避免精度误差
+## 无穷大表示:float('inf')
+## py 10**6 排序+双指针 3秒可能TLE
+## 按区间右端点排序,current.left>pre.right,贪心求不相交区间的最大个数
+## 加法>位运算
+def main():
+	t = int(input())
+	for i in range(t):
+		n = int(input())
+		a = list(map(int, input().split()))
+		mn, mx = [0] * n, [0] * n
+		s = set()
+		for j in range(n):
+			if j == 0 or a[j] != a[j - 1]:
+				mn[j], mx[j] = a[j], a[j]
+			s.add(a[j])
+		res = []
+		for x in range(1, n + 1):
+			if x not in s:
+				res.append(x)
+		index = 0
+		for j in range(n):
+			if mn[j] == 0:
+				mn[j] = res[index]
+				index += 1
+		stack = []
+		k = 1
+		for j in range(n):
+			if mx[j] != 0:
+				while k < mx[j]:
+					stack.append(k)
+					k += 1
+				k += 1
+			else:
+				v = stack.pop()
+				mx[j] = v
+		print(" ".join([str(x) for x in mn]))
+		print(" ".join([str(x) for x in mx]))
+
+
+if __name__ == "__main__":
+	main()
